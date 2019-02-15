435008dc4b261ce7ec923301763397553d8fd302
Keunbae Park <ppotpo@gmail.com>
2019-02-14 22:15:57 -0500

    wip

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light shadow mb-5">
     <div class="container py-3">
-        <a class="navbar-brand" href="#">Driveway Rental</a>
+        <a class="navbar-brand" href="#">PinParking</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
             aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
