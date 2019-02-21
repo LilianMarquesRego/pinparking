1888d44216b7327778c7127db6a48e7f0f4273da
Keunbae Park <ppotpo@gmail.com>
2019-02-21 14:30:31 -0500

    wip

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -15,7 +15,7 @@
     </div>
     <div class="footer-main bg-dark py-5 small">
         <div class="container">
-            <a href="#">Driveway Rental</a> is a project created and maintained by
+            <a href="#">PinParking</a> is a project created and maintained by
             <a href="#">Us</a>.
         </div>
     </div>
