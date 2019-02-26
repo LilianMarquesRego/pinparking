b1f7c9be380d8b9b8866d1374d3f0bc8a2ee2df0
Keunbae Park <ppotpo@gmail.com>
2019-02-26 16:40:52 -0500

    wip

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,6 @@
 
     <script>
         window.App = {!! json_encode([
-                'csrfToken' => csrf_token(),
                 'user' => Auth::user(),
                 'signedIn' => Auth::check()
             ]) !!};
