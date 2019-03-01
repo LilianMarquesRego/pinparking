25c5def49fe4ab581773c90b972f9882567ac6a5
Keunbae Park <ppotpo@gmail.com>
2019-03-01 07:29:37 -0500

    wip

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,6 +18,7 @@ $factory->define(App\User::class, function (Faker $faker) {
     return [
         'name' => $faker->name,
         'phone' => $faker->phoneNumber,
+        'age' => $faker->numberBetween(18, 60),
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
