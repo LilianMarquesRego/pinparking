ed9470bb596616670a6f35cf3f481f1644d3cb42
Keunbae Park <ppotpo@gmail.com>
2019-03-06 15:56:49 -0500

    wip

---
+++ chart/transactions.js
@@ -0,0 +1,126 @@
+let transactions = [
+    {
+        "transactions_count": 63,
+        "created_at": "2019-01-30 00:00:00"
+    },
+    {
+        "transactions_count": 66,
+        "created_at": "2019-01-31 00:00:00"
+    },
+    {
+        "transactions_count": 61,
+        "created_at": "2019-02-01 00:00:00"
+    },
+    {
+        "transactions_count": 64,
+        "created_at": "2019-02-02 00:00:00"
+    },
+    {
+        "transactions_count": 62,
+        "created_at": "2019-02-03 00:00:00"
+    },
+    {
+        "transactions_count": 71,
+        "created_at": "2019-02-04 00:00:00"
+    },
+    {
+        "transactions_count": 54,
+        "created_at": "2019-02-05 00:00:00"
+    },
+    {
+        "transactions_count": 69,
+        "created_at": "2019-02-06 00:00:00"
+    },
+    {
+        "transactions_count": 63,
+        "created_at": "2019-02-07 00:00:00"
+    },
+    {
+        "transactions_count": 65,
+        "created_at": "2019-02-08 00:00:00"
+    },
+    {
+        "transactions_count": 61,
+        "created_at": "2019-02-09 00:00:00"
+    },
+    {
+        "transactions_count": 64,
+        "created_at": "2019-02-10 00:00:00"
+    },
+    {
+        "transactions_count": 57,
+        "created_at": "2019-02-11 00:00:00"
+    },
+    {
+        "transactions_count": 61,
+        "created_at": "2019-02-12 00:00:00"
+    },
+    {
+        "transactions_count": 74,
+        "created_at": "2019-02-13 00:00:00"
+    },
+    {
+        "transactions_count": 62,
+        "created_at": "2019-02-14 00:00:00"
+    },
+    {
+        "transactions_count": 65,
+        "created_at": "2019-02-15 00:00:00"
+    },
+    {
+        "transactions_count": 56,
+        "created_at": "2019-02-16 00:00:00"
+    },
+    {
+        "transactions_count": 54,
+        "created_at": "2019-02-17 00:00:00"
+    },
+    {
+        "transactions_count": 70,
+        "created_at": "2019-02-18 00:00:00"
+    },
+    {
+        "transactions_count": 56,
+        "created_at": "2019-02-19 00:00:00"
+    },
+    {
+        "transactions_count": 66,
+        "created_at": "2019-02-20 00:00:00"
+    },
+    {
+        "transactions_count": 59,
+        "created_at": "2019-02-21 00:00:00"
+    },
+    {
+        "transactions_count": 69,
+        "created_at": "2019-02-22 00:00:00"
+    },
+    {
+        "transactions_count": 85,
+        "created_at": "2019-02-23 00:00:00"
+    },
+    {
+        "transactions_count": 61,
+        "created_at": "2019-02-24 00:00:00"
+    },
+    {
+        "transactions_count": 68,
+        "created_at": "2019-02-25 00:00:00"
+    },
+    {
+        "transactions_count": 64,
+        "created_at": "2019-02-26 00:00:00"
+    },
+    {
+        "transactions_count": 70,
+        "created_at": "2019-02-27 00:00:00"
+    },
+    {
+        "transactions_count": 77,
+        "created_at": "2019-02-28 00:00:00"
+    },
+    {
+        "transactions_count": 63,
+        "created_at": "2019-03-01 00:00:00"
+    }
+]
